8cfab8676f55956a67e3de62ea4cb306244f5621
unknown <aa098933967901@gmai.com>
2022-06-17 20:14:44 +0800

    upload

---
+++ index.php
@@ -0,0 +1,39 @@
+﻿<!DOCTYPE html>
+	<html>
+		<head>	
+			<body background='w.jpg'>
+			<//body background-size: 100% 100%;>
+			<//body background-position: top center;>
+			<body style=" background-repeat:no-repeat ;
+			background-size:100% 100%; 
+			background-attachment: fixed;">
+						
+			<meta charset ="UTF-8">
+		</head>
+			<body>
+
+			
+				<?php
+					for($i=0;$i<125;$i++){
+						echo '&nbsp;';
+					}
+				?>
+				<u><b><font color='white' size='10' face='DFKai-sb'>阿晟個人網站<br></font></b></u>
+				
+				<a href="stardy\妹塔\test1.php"><h2>妹塔</h2></a>
+				
+				<p><font color='white' size='6' face='DFKai-sb'>圖片連結</font></p>
+				<?php
+					echo '<br><br>';
+				?>
+				<?php //require 'footer.php';?>
+				<a href="stardy/3.php"3.php</a>
+				<img src="item0.png">
+				<p><font color='black'>meow meow~ (可惡被你發現了</font></p>
+				<?php
+					date_default_timezone_set('Asia/Taipei');
+					echo date('Y/m/d H/i/s');
+				?>
+			</body>
+	</html>
+
